758ace156e4074bac9322f66a87c8f8073ea0865
eskeemos <l.michalik004@gmail.com>
2023-04-16 16:39:15 +0200

    correct api path v2

--- client/src/pages/CreatePost.tsx
+++ client/src/pages/CreatePost.tsx
@@ -23,7 +23,7 @@ const CreatePost: React.FC<Props> = ({ }) => {
       setLoading(true);
 
       try {
-        const response = await fetch("http://localhost:8080/api/v1/post", {
+        const response = await fetch("https://eskeemos.onrender.com/api/v1/post", {
           method: "POST",
           headers: {
             "Content-Type": 'application/json'
