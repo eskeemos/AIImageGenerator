7932d734536f6a1f4d6bb93e70d3b57733ce4807
eskeemos <l.michalik004@gmail.com>
2023-04-16 14:37:04 +0200

    card logic add

--- client/src/components/Card.tsx
+++ client/src/components/Card.tsx
@@ -1,10 +1,33 @@
 import React from 'react'
+import { IoMdCodeDownload } from 'react-icons/io'
+import { downloadImage } from '../utils'
 
-type Props = {}
+type Props = {
+  _id: string,
+  name: string,
+  prompt: string,
+  photo: string
+}
 
-const Card = (props: Props) => {
+const Card = ({ _id, name, prompt, photo }: Props) => {
   return (
-    <div>Card</div>
+    <div className='rounded-xl group relative shadow-lg hover:shadow-2xl'>
+      <img src={photo} alt={prompt} className='w-full h-auto objecy-cover rounded-xl' />
+      <div className="group-hover:opacity-100 transition-opacity flex flex-col max-h-[95%] opacity-0 absolute bottom-0 left-0 right-0 m-2 p-4 rounded-md bg-violet-500">
+        <p className='text-white text-md overflow-y-auto'>{prompt}</p>
+        <div className="mt-5 flex justify-between items-center gap-2">
+          <div className="flex items-center gap-2">
+            <div className="uppercase w-7 h-7 rounded-full object-cover bg-white flex justify-center items-center font-bold">
+              {name[0]}
+            </div>
+            <p className='text-white text-sm'>{name}</p>
+          </div>
+          <button type='button' onClick={() => downloadImage(_id, photo)}>
+            <IoMdCodeDownload className='text-white text-2xl mr-2' />
+          </button>
+        </div>
+      </div>
+    </div>
   )
 }
 
--- client/src/utils/index.ts
+++ client/src/utils/index.ts
@@ -1,4 +1,5 @@
 import { surpriseMePrompts } from "../constants";
+import FileSaver from "file-saver";
 
 export function getRandomPrompt(prompt: string): string {
   const randomIndex = Math.floor(Math.random() * surpriseMePrompts.length);
@@ -7,4 +8,8 @@ export function getRandomPrompt(prompt: string): string {
   if (randomPrompt === prompt) return getRandomPrompt(prompt);
 
   return randomPrompt;
+}
+
+export async function downloadImage(_id: string, photo: string) {
+  FileSaver.saveAs(photo, `download-${_id}.jpg`);
 }
